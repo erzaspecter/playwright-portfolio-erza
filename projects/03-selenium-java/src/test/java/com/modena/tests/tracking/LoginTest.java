81962386076336c107298a1429545e4e67a90744
nama-anda <erza.akbar@modena.com>
2026-04-16 16:17:54 +0700

    update selenium java

--- projects/03-selenium-java/src/test/java/com/modena/tests/tracking/LoginTest.java
+++ projects/03-selenium-java/src/test/java/com/modena/tests/tracking/LoginTest.java
@@ -2,13 +2,21 @@ package com.modena.tests.tracking;
 
 import com.modena.base.BaseTest;
 import com.modena.pages.LoginPage;
+import com.modena.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
-    
+
     @Test
     public void testLoginSuccessfully() {
+        // 1. Inisialisasi Page Object
         LoginPage loginPage = new LoginPage(driver);
+        
+        // 2. Jalankan login (INI YANG MISSING!)
+        HomePage homePage = loginPage.loginApplication("tomsmith", "SuperSecretPassword!");
+        
+        // 3. Assertion: pastikan login berhasil (misal ada elemen welcome message)
+        Assert.assertNotNull(homePage, "HomePage should not be null after login");
     }
 }
